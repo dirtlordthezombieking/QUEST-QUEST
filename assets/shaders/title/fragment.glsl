cd02607502284c65fe2a2b3c57d8c728e0833b2c
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-11-30 16:41:44 -0400

    Update fragment.glsl

--- assets/shaders/title/fragment.glsl
+++ assets/shaders/title/fragment.glsl
@@ -4,6 +4,9 @@ uniform sampler2D u_tex;
 main()
 {
 	vec4 tex=texture2D(u_tex,v_uv);
-	float pos=((u_uv.x*0.25)+u_uv.y)*10)+(u_time-0.5)
-	gl_FragColor=
+	float pos=((u_uv.x*0.25)+u_uv.y)*10)
+	tex.x+=0.5*sin((pos*3.0)+(u_time*0.4));
+	tex.y+=sin((pos*5.0)+(u_time*0.6));
+	tex.z+=sin((pos*2.0)+(u_time*1.0));
+	gl_FragColor=tex;
 }
