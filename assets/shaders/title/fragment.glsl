08c4c4f45dee0a3db4eda913c1a02e32cc259c06
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-11-30 16:37:13 -0400

    Update fragment.glsl

--- assets/shaders/title/fragment.glsl
+++ assets/shaders/title/fragment.glsl
@@ -1,8 +1,9 @@
 varying vec2 v_uv;
-uniform float time;
+uniform float u_time;
 uniform sampler2D u_tex;
 main()
 {
 	vec4 tex=texture2D(u_tex,v_uv);
+	float pos=((u_uv.x*0.25)+u_uv.y)*10)+(u_time-0.5)
 	gl_FragColor=
 }
