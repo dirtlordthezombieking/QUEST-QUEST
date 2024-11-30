c7d50b62ea23b83404da364c988bdc2784f52dfe
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-11-30 19:14:35 -0400

    Update JavaScript

---
+++ JavaScript/loader.js
@@ -0,0 +1,28 @@
+const loaders=
+{
+	items:{},
+	async load(src)
+	{
+		//----
+	},
+	async loadString(src)
+	{
+		//----
+	},
+	async loadShader(src)
+	{
+		//----
+	},
+	async loadImage(src)
+	{
+		//----
+	},
+	async loadSound(src)
+	{
+		//----
+	},
+	async loadMusic(src)
+	{
+		//----
+	}
+};
