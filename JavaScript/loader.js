f583649d878e9643e677bcd53ebeb3a096171838
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-01 21:38:40 -0400

    Update loader.js

--- JavaScript/loader.js
+++ JavaScript/loader.js
@@ -73,9 +73,7 @@ const loader=
 			
 			out.indBuff=game.gl.createBuffer();
 			game.gl.bindBuffer(game.gl.ELEMENT_ARRAY_BUFFER,out.indBuff);
-			game.gl.bufferData(game.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(out.ind),
-    gl.STATIC_DRAW
-);
+			game.gl.bufferData(game.gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(out.ind),game.gl.STATIC_DRAW);
 		}
 		catch (e)
 		{
