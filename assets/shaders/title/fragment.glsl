bf0920af3a0455efa292b886d03c70642b6f4af7
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-02 16:01:54 -0400

    Update fragment.glsl

--- assets/shaders/title/fragment.glsl
+++ assets/shaders/title/fragment.glsl
@@ -6,9 +6,9 @@ void main()
 {
 	vec4 tex=texture2D(u_tex,v_uv);
 	float pos=((v_uv.x*4.0)+v_uv.y);
-	tex.x+=0.5*sin((pos*3.0)+(u_time*0.004));
-	tex.y+=sin((pos*5.0)+(u_time*0.006));
-	tex.z+=sin((pos*2.0)+(u_time*0.01));
+	tex.x+=0.05*sin((pos*3.0)+(u_time*0.0004));
+	tex.y+=0.1*sin((pos*5.0)+(u_time*0.0006));
+	tex.z+=0.1*sin((pos*2.0)+(u_time*0.001));
 	float a=tex.w;
 	tex.w=min(tex.w,(u_time*2.0)+(pos/5.0)-12.0);
 	tex.w=a;
