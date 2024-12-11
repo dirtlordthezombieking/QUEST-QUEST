55d7d97b29520e844ea358cf3c3174e41b7162f0
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-11 17:33:02 -0400

    Update gradient.js

--- tools/JavaScript/gradient.js
+++ tools/JavaScript/gradient.js
@@ -1,4 +1,11 @@
 function generate()
 {
-
+	const canvas=document.createElement("canvas");
+	canvas.width=200;
+	canvas.height=200;
+	const url=canvas.toDataURL();
+	const a=document.createElement('a');
+	a.download='gradient.png';
+	a.href=url;
+	a.textContent='Download PNG';
 }
