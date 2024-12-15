c1b33d968bf2ece586fa62824a24e29b216ae251
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-15 14:22:37 -0400

    Update fragment.glsl

--- assets/shaders/slide/fragment.glsl
+++ assets/shaders/slide/fragment.glsl
@@ -4,8 +4,9 @@ uniform sampler2D u_tex;
 uniform float u_time;
 void main()
 {
-	float r=texture2D(u_tex,v_uv+vec2(u_time,u_time)).x;
+	float time=u_time*0.064;
+	float r=texture2D(u_tex,v_uv+vec2(time,time)).x;
 	float g=texture2D(u_tex,v_uv).y;
-	float b=texture2D(u_tex,v_uv+vec2(u_time,-u_time)).z;
+	float b=texture2D(u_tex,v_uv+vec2(time,-time)).z;
 	gl_FragColor=vec2(r,g,b,1.0);
 }
