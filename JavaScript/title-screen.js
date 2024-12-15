f2557d111a935d895781fa84cf7403ae5357c393
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-15 13:16:39 -0400

    Update JavaScript

---
+++ JavaScript/title-screen.js
@@ -0,0 +1,26 @@
+const titleScreen=
+{
+	load()
+	{
+		
+		loader.loadMulti(
+		[
+			["misc/title.json","sprite"],
+			["misc/press f.json","sprite"],
+			["misc/PRESS SPAC.json","sprite"],
+			["misc/E TO START.json","sprite"]
+		]);
+	},
+	draw(d)
+	{
+		//----
+	},
+	keyDown(k)
+	{
+		//----
+	},
+	keyaup(k)
+	{
+		//----
+	}
+};
