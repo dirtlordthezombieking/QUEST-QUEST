767ed2742f935835ccd279600cc6671a26a8b6a6
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-15 13:29:36 -0400

    Update title-screen.js

--- JavaScript/title-screen.js
+++ JavaScript/title-screen.js
@@ -13,13 +13,16 @@ const titleScreen=
 	},
 	draw(d)
 	{
-		game.title.draw();
-		game.pressSpac.draw();
-		game.eToStart.draw();
+		titleScreen.title.draw();
+		titleScreen.pressSpac.draw();
+		titleScreen.eToStart.draw();
 	},
 	retrieve()
 	{
-		//----
+		titleScreen.title=loader.items["misc/title.json"].sprite.value;
+		game.pressF=loader.items["misc/press f.json"].sprite.value;
+		titleScreen.pressSpac=loader.items["misc/PRESS SPAC.json"].sprite.value;
+		titleScreen.eToStart=loader.items["misc/E TO START.json"].sprite.value;
 	},
 	keyDown(k)
 	{
