a230e3b3b9b42750eaedfddb80c2578f1a5a297b
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-16 14:00:29 -0400

    Update character-screen.js

--- JavaScript/character-screen.js
+++ JavaScript/character-screen.js
@@ -2,15 +2,15 @@ const characterScreen=
 {
 	texts:
 	[
-		["UI/text/character creation/body_type.png",],
+		["UI/text/character creation/body_type.png"    ,],
 		["UI/text/character creation/detail_colour.png",-384,],
-		["UI/text/character creation/detail_style.png",-],
-		["UI/text/character creation/hair_colour.png",-384,],
-		["UI/text/character creation/hair_style.png",-],
-		["UI/text/character creation/pants_colour.png",],
-		["UI/text/character creation/shirt_colour.png",],
-		["UI/text/character creation/shoe_colour.png",],
-		["UI/text/character creation/skin_tone.png",-],
+		["UI/text/character creation/detail_style.png" ,-352,],
+		["UI/text/character creation/hair_colour.png"  ,-384,],
+		["UI/text/character creation/hair_style.png"   ,-352,],
+		["UI/text/character creation/pants_colour.png" ,],
+		["UI/text/character creation/shirt_colour.png" ,],
+		["UI/text/character creation/shoe_colour.png"  ,],
+		["UI/text/character creation/skin_tone.png"    ,-352,],
 	]
 	load()
 	{
