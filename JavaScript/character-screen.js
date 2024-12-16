29074766aae90ee4abd056f462ba7ae91ec63b70
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-16 12:32:23 -0400

    Update character-screen.js

--- JavaScript/character-screen.js
+++ JavaScript/character-screen.js
@@ -46,6 +46,15 @@ const characterScreen=
 			]
 		),game.gl.STATIC_DRAW);
 		characterScreen.backTex=loader.items["UI/background.png"].texture.value;
+		//UI
+		characterScreen.basicShade=loader.items.basic.shader.value;
+		characterScreen.basicDataLoc=game.gl.getAttribLocation(characterScreen.basicShade,"a_data");
+		characterScreen.basicTexLoc=game.gl.getUniformLocation(characterScreen.basicShade,"u_tex");
+		characterScreen.basicOffLoc=game.gl.getUniformLocation(characterScreen.basicShade,"u_pos");
+		characterScreen.textShade=loader.items.text.shader.value;
+		characterScreen.textDataLoc=game.gl.getAttribLocation(characterScreen.textShade,"a_data");
+		characterScreen.textTexLoc=game.gl.getUniformLocation(characterScreen.textShade,"u_tex");
+		characterScreen.textColLoc=game.gl.getUniformLocation(characterScreen.textShade,"u_colour");
 	},
 	keyDown(k)
 	{
@@ -55,13 +64,11 @@ const characterScreen=
 	{
 		//----
 	},
-	createElement(x,y,w,h)
+	createElement(x,y,w,h,tex)
 	{
 		ret={}
 		ret.shader=loader.items.basic.shader.value;
-		ret.dataLoc=game.gl.getAttribLocation(ret.shader,"a_data");
-		ret.texLoc=game.gl.getUniformLocation(ret.shader,"u_tex");
-		ret.offLoc=game.gl.getUniformLocation(ret.shader,"u_pos");
+		rt.tex=tex;
 		ret.vertBuff=game.gl.createBuffer();
 		game.gl.bindBuffer(game.gl.ARRAY_BUFFER,ret.vertBuff);
 		game.gl.bufferData(game.gl.ARRAY_BUFFER,new Float32Array(
@@ -72,6 +79,15 @@ const characterScreen=
 					x+w,y+h
 				]
 		),game.gl.STATIC_DRAW);
-		
+		ret.draw=function()
+		{
+		game.setTexture(characterScreen.basicDataLoc,this.tex,0);
+		game.gl.bindBuffer(game.gl.ARRAY_BUFFER,characterScreen.backVertBuff);
+		game.gl.enableVertexAttribArray(characterScreen.backPosLoc);
+		game.gl.vertexAttribPointer(characterScreen.backPosLoc,2,game.gl.FLOAT,false,0,0);
+		game.gl.uniform1f(characterScreen.backTimeLoc,t%5000);
+		game.gl.bindBuffer(game.gl.ELEMENT_ARRAY_BUFFER,characterScreen.indBuff);
+		game.gl.drawElements(game.gl.TRIANGLES,6,game.gl.UNSIGNED_SHORT,0);
+		}
 	},
 };
