7ee21a2df0328c479dde49d7216e49fc0956fd03
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-20 16:43:36 -0400

    Update character-screen.js

--- JavaScript/character-screen.js
+++ JavaScript/character-screen.js
@@ -193,6 +193,7 @@ const characterScreen=
 			["UI/text/character creation/race/nephilim.png","texture"],
 			["UI/text/character creation/race/vampire.png","texture"]
 		]);
+		characterRenderer.load();
 	},
 	draw(d,t)
 	{
@@ -298,10 +299,15 @@ const characterScreen=
 			characterScreen.drawHex[n]();
 		}
 		characterScreen.races[characterScreen.race].draw();
+		characterRenderer.draw(characterScreen.char,-70,  8,0,(t%1000)/250);
+		characterRenderer.draw(characterScreen.char,  8,  8,1,(t%1000)/250);
+		characterRenderer.draw(characterScreen.char,-70,-70,2,(t%1000)/250);
+		characterRenderer.draw(characterScreen.char,  8,-70,3,(t%1000)/250);
 	},
 	retrieve()
 	{
 		//BACKGROUND
+		characterRenderer.retrive();
 		characterScreen.backShade=loader.items.slide.shader.value;
 		characterScreen.backPosLoc=game.gl.getAttribLocation(characterScreen.backShade,"a_pos");
 		characterScreen.backTexLoc=game.gl.getUniformLocation(characterScreen.backShade,"u_tex");
