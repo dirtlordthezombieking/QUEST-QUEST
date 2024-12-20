c5d18f9e703be06abe8b9bba2867a8cbfe1364ad
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-20 17:44:50 -0400

    Update characterRenderer.js

--- JavaScript/characterRenderer.js
+++ JavaScript/characterRenderer.js
@@ -7,27 +7,46 @@ const characterRenderer=
 		loader.loadMulti(
 		[
 			["character","shader"],
-			["sprite/detail/beastfolk/fur/001.png","texture"],
+
+			["sprite/detail/beastfolk/fur/001.png" ,"texture"],
 			["sprite/detail/beastfolk/skin/001.png","texture"],
-			["sprite/detail/demons/horns/001.png","texture"],
-			["sprite/detail/fishfolk/back/00.png","texture"],
-			["sprite/detail/fishfolk/back/01.png","texture"],
-			["sprite/detail/fishfolk/back/10.png","texture"],
-			["sprite/detail/fishfolk/back/11.png","texture"],
-			["sprite/detail/nephilim/halo/001.png","texture"],
-			["sprite/detail/vampire/fangs/001.png","texture"],
-			["sprite/detail/vampire/fangs/002.png","texture"],
-			["sprite/ears/.png","texture"],
-			["sprite/eyebrows/.png","texture"],
-			["sprite/eyes/.png,"texture"],
-			["sprite/hair/.png","texture"],
-			["sprite/pants/.png","texture"],
-			["sprite/shirt/001/.png","texture"],
-			["sprite/shoes/","texture"],
-			["sprite/00.png","texture"],
-			["sprite/01.png","texture"],
-			["sprite/10.png","texture"],
-			["sprite/11.png","texture"]
+			["sprite/detail/demons/horns/001.png"  ,"texture"],
+			["sprite/detail/fishfolk/back/00.png"  ,"texture"],
+			["sprite/detail/fishfolk/back/01.png"  ,"texture"],
+			["sprite/detail/fishfolk/back/10.png"  ,"texture"],
+			["sprite/detail/fishfolk/back/11.png"  ,"texture"],
+			["sprite/detail/nephilim/halo/001.png" ,"texture"],
+			["sprite/detail/vampire/fangs/001.png" ,"texture"],
+			["sprite/detail/vampire/fangs/002.png" ,"texture"],
+
+			["sprite/ears/default.png"             ,"texture"],
+			["sprite/ears/none.png"                ,"texture"],
+			["sprite/ears/pointy.png"              ,"texture"],
+
+			["sprite/eyebrows/normal.png"          ,"texture"],
+			["sprite/eyebrows/thick.png"           ,"texture"],
+
+			["sprite/eyes/base/001.png"            ,"texture"],
+			["sprite/eyes/base/001.png"            ,"texture"],
+
+			["sprite/hair/001.png"                 ,"texture"],
+			["sprite/hair/002.png"                 ,"texture"],
+			["sprite/hair/front/001.png"           ,"texture"],
+			["sprite/hair/front/002.png"           ,"texture"],
+
+			["sprite/pants/basic.png"              ,"texture"],
+
+			["sprite/shirt/001/00.png"             ,"texture"],
+			["sprite/shirt/001/01.png"             ,"texture"],
+			["sprite/shirt/001/10.png"             ,"texture"],
+			["sprite/shirt/001/11.png"             ,"texture"],
+
+			["sprite/shoes/basic"                  ,"texture"],
+
+			["sprite/00.png"                       ,"texture"],
+			["sprite/01.png"                       ,"texture"],
+			["sprite/10.png"                       ,"texture"],
+			["sprite/11.png"                       ,"texture"]
 		]);
 	},
 	retrieve()
