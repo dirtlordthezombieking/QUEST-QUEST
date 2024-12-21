021cc286e007165fb37725600ae7239396d04507
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-21 13:08:28 -0400

    Update characterRenderer.js

--- JavaScript/characterRenderer.js
+++ JavaScript/characterRenderer.js
@@ -461,7 +461,7 @@ const characterRenderer=
 	},
 	draw(data,x,y,dir,frame)
 	{
-		characterRenderer.methods[data.race],(data,x,y,characterRenderer.frames[frame],dir/4);
+		characterRenderer.methods[data.race](data,x,y,characterRenderer.frames[frame],dir/4);
 	},
 	drawPart(texture,colour)
 	{
