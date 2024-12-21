c6fbfd2c565f4e1546761611616dd4ca4e5c3be2
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-21 17:20:24 -0400

    Update detail-screen.js

--- JavaScript/detail-screen.js
+++ JavaScript/detail-screen.js
@@ -1,2 +1,23 @@
 //_____'(s|re) going to eat the food. oh _____, _____ got it _____self so it's _____s.
 //blank
+const detailScreen=
+{
+	load()
+	{
+		loader.loadMulti(
+		[
+		]);
+	},
+	draw(d,t)
+	{
+	},
+	retrieve()
+	{
+	},
+	keyDown(k)
+	{
+	},
+	keyUp(k)
+	{
+	}
+};
