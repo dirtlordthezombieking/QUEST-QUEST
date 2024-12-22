ae57c6d2bce58b948cff545788f6ded44abe84b3
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-22 18:42:03 -0400

    Update vertex.glsl

--- assets/shaders/font/vertex.glsl
+++ assets/shaders/font/vertex.glsl
@@ -1,8 +1,8 @@
 attribute vec4 a_data;
 varying vec2 v_uv;
-uniform vec2 u_data;
+attribute vec4 a_char;
 void main()
 {
-	v_uv=a_data.zw+a_data.zw;
-	gl_Position=vec4((a_data.xy+a_data.xy)/vec2(384.0,256.0),0.0,1.0);
+	v_uv=a_data.zw+a_char.zw;
+	gl_Position=vec4((a_data.xy+a_char.xy)/vec2(384.0,256.0),0.0,1.0);
 }
