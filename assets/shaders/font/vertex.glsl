2b323cd533e6621aecb9a473f1abe07fda730f04
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-22 15:27:31 -0400

    Update shaders

---
+++ assets/shaders/font/vertex.glsl
@@ -0,0 +1,8 @@
+attribute vec4 a_data;
+varying vec2 v_uv;
+uniform vec4[500] u_data;
+void main()
+{
+	v_uv=a_data.zw+u_data[].zw;
+	gl_Position=vec4((a_data.xy+u_data[].xy)/vec2(384.0,256.0),0.0,1.0);
+}
