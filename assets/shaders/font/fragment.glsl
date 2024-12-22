3f0a11d6446ca85afc5ffa81e9ef1b139ebf8bbb
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-22 15:31:09 -0400

    Update font

---
+++ assets/shaders/font/fragment.glsl
@@ -0,0 +1,10 @@
+precision mediump float;
+varying vec2 v_uv;
+uniform vec3 u_colour;
+uniform sampler2D u_tex;
+void main()
+{
+	vec4 tex=texture2D(u_tex,v_uv);
+	vec4 col=vec4(u_colour.x*tex.x/255.0,u_colour.y*tex.x/255.0,u_colour.z*tex.x/255.0,tex.y);
+	gl_FragColor=col;
+}
