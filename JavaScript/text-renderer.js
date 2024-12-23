5b093aff181aacfba6c7e103e0cca99c3f4b642a
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-22 21:50:02 -0400

    Update text-renderer.js

--- JavaScript/text-renderer.js
+++ JavaScript/text-renderer.js
@@ -49,9 +49,9 @@ const textRenderer=
 				0,16,0.0000,0.125,
 				8, 0,0.0625,0.000,
 				8,16,0.0625,0.125,
-			]
-			),game.gl.STATIC_DRAW);
+			]),game.gl.STATIC_DRAW);
 			textRenderer.charBuff=game.gl.createBuffer();
+		}
 	},
 	draw(colour)
 	{
