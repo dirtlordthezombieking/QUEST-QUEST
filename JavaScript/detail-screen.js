e37349d1d3adfafc50c9bc5651545e078e9780d7
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-23 12:23:33 -0400

    Update detail-screen.js

--- JavaScript/detail-screen.js
+++ JavaScript/detail-screen.js
@@ -3,6 +3,7 @@
 //0123456701234567012345670123456701234567012345670123456701234567
 const detailScreen=
 {
+	choice:0,
 	load()
 	{
 		textRenderer.load();
@@ -39,5 +40,150 @@ const detailScreen=
 		"__",
 		"_",
 		""
+	],
+	set()
+	{
+		switch(detailScreen.choice)
+		{
+			case 0:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+add[list[0][1].length]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+
+				" food.\noh, "+
+				list[2][1]+
+				", "+
+				list[3][0]+
+				" got it "+
+				list[4][0]+
+				"self so it's "+
+				list[5][0]+
+				"."+,
+				2));
+				break;
+			case 1:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+add[list[][0].length]+
+				" food.\noh, "+
+				list[2][1]+
+				", "+
+				list[3][0]+
+				" got it "+
+				list[4][0]+
+				"self so it's "+
+				list[5][0]+
+				"."+,
+				2));
+				break;
+			case 2:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+
+				" food.\noh, "+
+				list[2][1]+add[list[2][1].length]+
+				", "+
+				list[3][0]+
+				" got it "+
+				list[4][0]+
+				"self so it's "+
+				list[5][0]+
+				"."+,
+				2));
+				break;
+			case 3:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+
+				" food.\noh, "+
+				list[2][1]+
+				", "+
+				list[3][0]+add[list[3][0].length]+
+				" got it "+
+				list[4][0]+
+				"self so it's "+
+				list[5][0]+
+				"."+,
+				2));
+				break;
+			case 4:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+
+				" food.\noh, "+
+				list[2][1]+
+				", "+
+				list[3][0]+
+				" got it "+
+				list[4][0]+add[list[4][0].length]+
+				"self so it's "+
+				list[5][0]+
+				"."+,
+				2));
+				break;
+			case 5:
+				textRenderer.setBlock(textRenderer.createBlock(-256,0,
+				list[0][1]+tail[list[0][2]]+
+				" going to eat "+
+				list[1][0]+
+				" food.\noh, "+
+				list[2][1]+
+				", "+
+				list[3][0]+
+				" got it "+
+				list[4][0]+
+				"self so it's "+
+				list[5][0]+add[list[5][0].length]+
+				"."+,
+				2));
+				break;
+		}
+	},
+	keys:
+	[
+		"KeyA":["A","a"],
+		"KeyB":["B","b"],
+		"KeyC":["C","c"],
+		"KeyD":["D","d"],
+		"KeyE":["E","e"],
+		"KeyF":["F","f"],
+		"KeyG":["G","g"],
+		"KeyH":["H","h"],
+		"KeyI":["I","i"],
+		"KeyJ":["J","j"],
+		"KeyK":["K","k"],
+		"KeyL":["L","l"],
+		"KeyM":["M","m"],
+		"KeyN":["N","n"],
+		"KeyO":["O","o"],
+		"KeyP":["P","p"],
+		"KeyQ":["Q","q"],
+		"KeyR":["R","r"],
+		"KeyS":["S","s"],
+		"KeyT":["T","t"],
+		"KeyU":["U","u"],
+		"KeyV":["V","v"],
+		"KeyW":["W","w"],
+		"KeyX":["X","x"],
+		"KeyY":["Y","y"],
+		"KeyZ":["Z","z"],
+	],
+	tail:
+	[
+		"'s"
+		"'re"
+	],
+	list:
+	[
+		["________","________",0],
+		["________","________"],
+		["________","________"],
+		["________","________"],
+		["________","________"],
+		["________","________"]
 	]
 };
