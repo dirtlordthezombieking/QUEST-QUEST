d7d8ef81fc26668835128f6657e7258decfb1b24
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-23 13:24:15 -0400

    Update fragment.glsl

--- assets/shaders/font/fragment.glsl
+++ assets/shaders/font/fragment.glsl
@@ -1,10 +1,10 @@
 precision mediump float;
 varying vec2 v_uv;
-uniform vec3 u_colour;
+uniform vec4 u_colour;
 uniform sampler2D u_tex;
 void main()
 {
 	vec4 tex=texture2D(u_tex,v_uv);
-	vec4 col=vec4((u_colour.x*tex.x/255.0),u_colour.y*tex.x/255.0,u_colour.z*tex.x/255.0,tex.y);
+	vec4 col=vec4(u_colour.x*tex.x/255.0,u_colour.y*tex.x/255.0,u_colour.z*tex.x/255.0,tex.y*u_colour.w/255.0);
 	gl_FragColor=col;
 }
