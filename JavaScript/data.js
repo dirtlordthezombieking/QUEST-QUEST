b89f9bd17a2b31061d9f3254844c562d1ee34841
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-24 16:16:03 -0400

    Update data.js

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -2,16 +2,28 @@ const data={}
 {
 	update(obj,path,value)
 	{
+		if(!data[obj])
+		{
+			data[obj]={};
+		}
 		let hold=data[obj];
 		for(let i=0;i<path.length-1;i++)
 		{
 			if(!hold[path[i])
 			{
-				
+				if((typeof path[i])=="string")
+				{
+					hold[path[i]]={};
+				}
+				else if((typeof path[i])=="number")
+				{
+					hold[path[i]]=[];
+				}
 			}
+			hold=hold[path.[i]];
 		}
-		hold[path.length-1]=value:
-		window.localStorage.setItem(obj)
+		hold[path.length-1]=value;
+		window.localStorage.setItem(obj,JSON.stringify(data[obj]));
 	}
 	load()
 	{
