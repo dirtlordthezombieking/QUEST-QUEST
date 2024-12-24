97977345192f086d0e55f99884f1ee7f926f6fef
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-24 16:06:14 -0400

    Update data.js

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -1,11 +1,20 @@
 const data={}
 {
-	save(dest,src)
+	update(obj,path,value)
 	{
-		window.localStorage.setItem(dest,src)
+		let hold=data[obj];
+		for(let i=0;i<path.length-1;i++)
+		{
+			if(!hold[path[i])
+			{
+				
+			}
+		}
+		hold[path.length-1]=value:
+		window.localStorage.setItem(obj)
 	}
 	load()
 	{
-		
+		data.devOptions=window.localStorage.setItem("devOptions","devOptions");
 	}
 }
