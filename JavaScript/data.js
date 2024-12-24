665bdb04fe56e5e2728176743c141501c71b6de7
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2024-12-24 16:31:39 -0400

    Update data.js

--- JavaScript/data.js
+++ JavaScript/data.js
@@ -1,3 +1,4 @@
+//const game={};
 const data=
 {
 	update(obj,path,value)
@@ -11,23 +12,44 @@ const data=
 		{
 			if(!hold[path[i]])
 			{
-				if((typeof path[i])=="string")
+				if((typeof path[i+1])=="string")
 				{
 					hold[path[i]]={};
 				}
-				else if((typeof path[i])=="number")
+				else if((typeof path[i+1])=="number")
 				{
 					hold[path[i]]=[];
 				}
 			}
 			hold=hold[path[i]];
 		}
-		hold[path.length-1]=value;
+		hold[path[path.length-1]]=value;
 		window.localStorage.setItem(obj,JSON.stringify(data[obj]));
 		game.log.inform(JSON.stringify(data[obj]));
+	},
+	get(obj,path)
+	{
+		if(!data[obj])
+		{
+			return undefined;
+		}
+		let hold=data[obj];
+		for(let i=0;i<path.length-1;i++)
+		{
+			if(!hold[path[i]])
+			{
+				return undefined;
+			}
+			hold=hold[path[i]];
+		}
+		return hold[path[path.length-1]];
 	},
 	load()
 	{
 		data.devOptions=window.localStorage.setItem("devOptions","devOptions");
+		if(data.get("devOptions",["devMode"]))
+		{
+			game.log.inform("loaded in Dev mode");
+		}
 	}
 };
