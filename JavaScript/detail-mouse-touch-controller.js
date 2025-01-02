5868f223408672916a243e041855741773f286da
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-02 12:34:34 -0400

    Update detail-mouse-touch-controller.js

--- JavaScript/detail-mouse-touch-controller.js
+++ JavaScript/detail-mouse-touch-controller.js
@@ -2,6 +2,7 @@
 //const utils={};
 const detailMouseTouchController=
 {
+	chars=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 	touchMouseStart(x,y)
 	{
 	},
@@ -10,5 +11,9 @@ const detailMouseTouchController=
 	},
 	touchMouseEnd(x,y)
 	{
+		if(x<-256)
+		{
+			
+		}
 	},
 };
