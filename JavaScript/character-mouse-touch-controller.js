2e381d9ecf8d6f76b2fe9be4bef1affcf064f80f
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-02 11:44:43 -0400

    Update character-mouse-touch-controller.js

--- JavaScript/character-mouse-touch-controller.js
+++ JavaScript/character-mouse-touch-controller.js
@@ -97,27 +97,27 @@ const characterMouseTouchController=
 					{
 						if(x<-304)
 						{
-							characterScreen.char.detailColour[0]=utils.clamp(characterScreen.char.hairColour[0]+(16*add),0,255);
+							characterScreen.char.detailColour[0]=utils.clamp(characterScreen.char.detailColour[0]+(16*add),0,255);
 						}
 						else if(x<-296)
 						{
-							characterScreen.char.detailColour[0]=utils.clamp(characterScreen.char.hairColour[0]+add,0,255);
+							characterScreen.char.detailColour[0]=utils.clamp(characterScreen.char.detailColour[0]+add,0,255);
 						}
 						else if(x<-288)
 						{
-							characterScreen.char.detailColour[1]=utils.clamp(characterScreen.char.hairColour[1]+(16*add),0,255);
+							characterScreen.char.detailColour[1]=utils.clamp(characterScreen.char.detailColour[1]+(16*add),0,255);
 						}
 						else if(x<-280)
 						{
-							characterScreen.char.detailColour[1]=utils.clamp(characterScreen.char.hairColour[1]+add,0,255);
+							characterScreen.char.detailColour[1]=utils.clamp(characterScreen.char.detailColour[1]+add,0,255);
 						}
 						else if(x<-272)
 						{
-							characterScreen.char.detailColour[2]=utils.clamp(characterScreen.char.hairColour[2]+(16*add),0,255);
+							characterScreen.char.detailColour[2]=utils.clamp(characterScreen.char.detailColour[2]+(16*add),0,255);
 						}
 						else if(x<-264)
 						{
-							characterScreen.char.detailColour[2]=utils.clamp(characterScreen.char.hairColour[2]+add,0,255);
+							characterScreen.char.detailColour[2]=utils.clamp(characterScreen.char.detailColour[2]+add,0,255);
 						}
 					}
 				}
@@ -171,27 +171,27 @@ const characterMouseTouchController=
 					{
 						if(x<336)
 						{
-							characterScreen.char.shirtColour[0]=utils.clamp(characterScreen.char.hairColour[0]+(16*add),0,255);
+							characterScreen.char.shirtColour[0]=utils.clamp(characterScreen.char.shirtColour[0]+(16*add),0,255);
 						}
 						else if(x<344)
 						{
-							characterScreen.char.shirtColour[0]=utils.clamp(characterScreen.char.hairColour[0]+add,0,255);
+							characterScreen.char.shirtColour[0]=utils.clamp(characterScreen.char.shirtColour[0]+add,0,255);
 						}
 						else if(x<352)
 						{
-							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.hairColour[1]+(16*add),0,255);
+							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.shirtColour[1]+(16*add),0,255);
 						}
 						else if(x<360)
 						{
-							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.hairColour[1]+add,0,255);
+							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.shirtColour[1]+add,0,255);
 						}
 						else if(x<368)
 						{
-							characterScreen.char.shirtColour[2]=utils.clamp(characterScreen.char.hairColour[2]+(16*add),0,255);
+							characterScreen.char.shirtColour[2]=utils.clamp(characterScreen.char.shirtColour[2]+(16*add),0,255);
 						}
 						else if(x<376)
 						{
-							characterScreen.char.shirtColour[2]=utils.clamp(characterScreen.char.hairColour[2]+add,0,255);
+							characterScreen.char.shirtColour[2]=utils.clamp(characterScreen.char.shirtColour[2]+add,0,255);
 						}
 					}
 				}
@@ -208,27 +208,27 @@ const characterMouseTouchController=
 					{
 						if(x<336)
 						{
-							characterScreen.char.pantsColour[0]=utils.clamp(characterScreen.char.hairColour[0]+(16*add),0,255);
+							characterScreen.char.pantsColour[0]=utils.clamp(characterScreen.char.pantsColour[0]+(16*add),0,255);
 						}
 						else if(x<344)
 						{
-							characterScreen.char.pantsColour[0]=utils.clamp(characterScreen.char.hairColour[0]+add,0,255);
+							characterScreen.char.pantsColour[0]=utils.clamp(characterScreen.char.pantsColour[0]+add,0,255);
 						}
 						else if(x<352)
 						{
-							characterScreen.char.pantsColour[1]=utils.clamp(characterScreen.char.hairColour[1]+(16*add),0,255);
+							characterScreen.char.pantsColour[1]=utils.clamp(characterScreen.char.pantsColour[1]+(16*add),0,255);
 						}
 						else if(x<360)
 						{
-							characterScreen.char.pantsColour[1]=utils.clamp(characterScreen.char.hairColour[1]+add,0,255);
+							characterScreen.char.pantsColour[1]=utils.clamp(characterScreen.char.pantsColour[1]+add,0,255);
 						}
 						else if(x<368)
 						{
-							characterScreen.char.pantsColour[2]=utils.clamp(characterScreen.char.hairColour[2]+(16*add),0,255);
+							characterScreen.char.pantsColour[2]=utils.clamp(characterScreen.char.pantsColour[2]+(16*add),0,255);
 						}
 						else if(x<376)
 						{
-							characterScreen.char.pantsColour[2]=utils.clamp(characterScreen.char.hairColour[2]+add,0,255);
+							characterScreen.char.pantsColour[2]=utils.clamp(characterScreen.char.pantsColour[2]+add,0,255);
 						}
 					}
 				}
@@ -245,27 +245,27 @@ const characterMouseTouchController=
 					{
 						if(x<336)
 						{
-							characterScreen.char.shoeColour[0]=utils.clamp(characterScreen.char.hairColour[0]+(16*add),0,255);
+							characterScreen.char.shoeColour[0]=utils.clamp(characterScreen.char.shoeColour[0]+(16*add),0,255);
 						}
 						else if(x<344)
 						{
-							characterScreen.char.shoeColour[0]=utils.clamp(characterScreen.char.hairColour[0]+add,0,255);
+							characterScreen.char.shoeColour[0]=utils.clamp(characterScreen.char.shoeColour[0]+add,0,255);
 						}
 						else if(x<352)
 						{
-							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.hairColour[1]+(16*add),0,255);
+							characterScreen.char.shirtColour[1]=utils.clamp(characterScreen.char.shoeColour[1]+(16*add),0,255);
 						}
 						else if(x<360)
 						{
-							characterScreen.char.shoeColour[1]=utils.clamp(characterScreen.char.hairColour[1]+add,0,255);
+							characterScreen.char.shoeColour[1]=utils.clamp(characterScreen.char.shoeColour[1]+add,0,255);
 						}
 						else if(x<368)
 						{
-							characterScreen.char.shoeColour[2]=utils.clamp(characterScreen.char.hairColour[2]+(16*add),0,255);
+							characterScreen.char.shoeColour[2]=utils.clamp(characterScreen.char.shoeColour[2]+(16*add),0,255);
 						}
 						else if(x<376)
 						{
-							characterScreen.char.shoeColour[2]=utils.clamp(characterScreen.char.hairColour[2]+add,0,255);
+							characterScreen.char.shoeColour[2]=utils.clamp(characterScreen.char.shoeColour[2]+add,0,255);
 						}
 					}
 				}
@@ -295,27 +295,27 @@ const characterMouseTouchController=
 			{
 				if(x<-176)
 				{
-					characterScreen.char.eyeColour[0]=utils.clamp(characterScreen.char.hairColour[0]+(16*add),0,255);
+					characterScreen.char.eyeColour[0]=utils.clamp(characterScreen.char.eyeColour[0]+(16*add),0,255);
 				}
 				else if(x<-168)
 				{
-					characterScreen.char.eyeColour[0]=utils.clamp(characterScreen.char.hairColour[0]+add,0,255);
+					characterScreen.char.eyeColour[0]=utils.clamp(characterScreen.char.eyeColour[0]+add,0,255);
 				}
 				else if(x<-160)
 				{
-					characterScreen.char.eyeColour[1]=utils.clamp(characterScreen.char.hairColour[1]+(16*add),0,255);
+					characterScreen.char.eyeColour[1]=utils.clamp(characterScreen.char.eyeColour[1]+(16*add),0,255);
 				}
 				else if(x<-152)
 				{
-					characterScreen.char.eyeColour[1]=utils.clamp(characterScreen.char.hairColour[1]+add,0,255);
+					characterScreen.char.eyeColour[1]=utils.clamp(characterScreen.char.eyeColour[1]+add,0,255);
 				}
 				else if(x<-144)
 				{
-					characterScreen.char.eyeColour[2]=utils.clamp(characterScreen.char.hairColour[2]+(16*add),0,255);
+					characterScreen.char.eyeColour[2]=utils.clamp(characterScreen.char.eyeColour[2]+(16*add),0,255);
 				}
 				else if(x<-136)
 				{
-					characterScreen.char.eyeColour[2]=utils.clamp(characterScreen.char.hairColour[2]+add,0,255);
+					characterScreen.char.eyeColour[2]=utils.clamp(characterScreen.char.eyeColour[2]+add,0,255);
 				}
 			}
 		}
