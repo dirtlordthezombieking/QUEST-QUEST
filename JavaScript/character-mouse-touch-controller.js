4f56b087c9e54b9c23fb3a1e317f6ab38ab48c24
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-02 10:58:29 -0400

    Update character-mouse-touch-controller.js

--- JavaScript/character-mouse-touch-controller.js
+++ JavaScript/character-mouse-touch-controller.js
@@ -39,7 +39,7 @@ const characterMouseTouchController=
 			//game.log.inform(""+characterScreen.time+"|"+characterMouseTouchController.lastClick+"|"+(characterScreen.time-characterMouseTouchController.lastClick));
 		}
 		//game.log.inform(""+characterScreen.time+"|"+characterMouseTouchController.lastClick+"|"+(characterScreen.time-characterMouseTouchController.lastClick));
-		game.log.inform(""+x+","+y);
+		//game.log.inform(""+x+","+y);
 		characterMouseTouchController.lastClick=characterScreen.time;
 		if(x<-256)
 		{
@@ -292,58 +292,58 @@ const characterMouseTouchController=
 				characterScreen.skinTone=utils.clamp((x+368)*(130/96),0,130);
 				break;
 			case 1:
-				characterScreen.char.hairColour[0]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.hairColour[0]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 2:
-				characterScreen.char.hairColour[1]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.hairColour[1]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 3:
-				characterScreen.char.hairColour[2]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.hairColour[2]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 4:
-				characterScreen.char.detailColour[0]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.detailColour[0]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 5:
-				characterScreen.char.detailColour[1]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.detailColour[1]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 6:
-				characterScreen.char.detailColour[2]=utils.clamp((x+368)*2.65625)0,255);
+				characterScreen.char.detailColour[2]=utils.clamp((x+368)*2.65625,0,255);
 				break;
 			case 7:
-				characterScreen.char.shirtColour[0]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shirtColour[0]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 8:
-				characterScreen.char.shirtColour[1]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shirtColour[1]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 9:
-				characterScreen.char.shirtColour[2]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shirtColour[2]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 10:
-				characterScreen.char.pantsColour[0]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.pantsColour[0]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 11:
-				characterScreen.char.pantsColour[1]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.pantsColour[1]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 12:
-				characterScreen.char.pantsColour[2]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.pantsColour[2]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 13:
-				characterScreen.char.shoeColour[0]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shoeColour[0]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 14:
-				characterScreen.char.shoeColour[1]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shoeColour[1]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 15:
-				characterScreen.char.shoeColour[2]=utils.clamp((x-272)*2.65625)0,255);
+				characterScreen.char.shoeColour[2]=utils.clamp((x-272)*2.65625,0,255);
 				break;
 			case 16:
-				characterScreen.char.eyeColour[0]=utils.clamp((x+176)*2.65625)0,255);
+				characterScreen.char.eyeColour[0]=utils.clamp((x+176)*2.65625,0,255);
 				break;
 			case 17:
-				characterScreen.char.eyeColour[1]=utils.clamp((x+48)*2.65625)0,255);
+				characterScreen.char.eyeColour[1]=utils.clamp((x+48)*2.65625,0,255);
 				break;
 			case 18:
-				characterScreen.char.eyeColour[2]=utils.clamp((x-80)*2.65625)0,255);
+				characterScreen.char.eyeColour[2]=utils.clamp((x-80)*2.65625,0,255);
 				break;
 		}
 	}
