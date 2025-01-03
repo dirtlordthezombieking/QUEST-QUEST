c369f7c9803daabbdf030e934ae54e6ad488cfee
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-03 10:47:02 -0400

    Update text-renderer.js

--- JavaScript/text-renderer.js
+++ JavaScript/text-renderer.js
@@ -68,10 +68,10 @@ const textRenderer=
 			game.gl.bindBuffer(game.gl.ARRAY_BUFFER,textRenderer.charBuff);
 			game.gl.enableVertexAttribArray(textRenderer.charLoc);
 			game.gl.vertexAttribPointer(textRenderer.charLoc,4,game.gl.FLOAT,false,0,0);
-			game.ext.vertexAttribDivisorANGLE(textRenderer.charLoc,1);
+			game.gl.vertexAttribDivisor(textRenderer.charLoc,1);
 			game.gl.uniform4f(textRenderer.colLoc,colour[0],colour[1],colour[2],colour[3]);
 			game.gl.bindBuffer(game.gl.ELEMENT_ARRAY_BUFFER,game.indS);
-			game.ext.drawElementsInstancedANGLE(game.gl.TRIANGLES,6,game.gl.UNSIGNED_SHORT,0,textRenderer.charCount);
+			game.gl.drawElementsInstanced(game.gl.TRIANGLES,6,game.gl.UNSIGNED_SHORT,0,textRenderer.charCount);
 		}
 	},
 	setBlock(b)
