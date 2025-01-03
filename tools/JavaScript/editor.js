d48193b588a9a2f44148c329dbeca809872ad8d9
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-03 12:49:21 -0400

    Update editor.js

--- tools/JavaScript/editor.js
+++ tools/JavaScript/editor.js
@@ -8,4 +8,16 @@ gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
 gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
 gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
-layers=[]
+width=10;
+height=10;
+let layers=[];
+function generate()
+{
+	layers=[];
+}
+addLayer()
+{
+	for(let i=0;i<height*height)
+	{
+	}
+}
