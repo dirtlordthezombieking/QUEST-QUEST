5ea02d2b17140249a4599c20774ad01368b43db0
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-03 14:59:06 -0400

    Update pixel.js

--- tools/JavaScript/pixel.js
+++ tools/JavaScript/pixel.js
@@ -1,10 +1,14 @@
 canvas=document.querySelector("#canvas");
 gl=canvas.getContext("webgl2",{premultipliedAlpha:false});
-canvas.width=canvas.clientWidth;
-canvas.height=canvas.clientHeight;
 gl.enable(gl.CULL_FACE);
 gl.enable(gl.BLEND);
 gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);
 gl.viewport(0,0,gl.canvas.width,gl.canvas.height);
 gl.clearColor(0,0,0,1);
 gl.clear(gl.COLOR_BUFFER_BIT);
+function resizeCanvas()
+{
+	canvas.width=canvas.clientWidth;
+	canvas.height=canvas.clientHeight;
+}
+new ResizeObserver(resizeCanvas).observe(canvas);
