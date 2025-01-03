0cf1196856ef3923c697c081c37c2ad7196322c7
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-03 10:49:03 -0400

    Update fragment.glsl

--- assets/shaders/basic/fragment.glsl
+++ assets/shaders/basic/fragment.glsl
@@ -1,8 +1,10 @@
+#version 300 es
+out vec4 fragColor;
 precision mediump float;
-varying vec2 v_uv;
+in vec2 v_uv;
 uniform sampler2D u_tex;
 void main()
 {
 	vec4 tex=texture2D(u_tex,v_uv);
-	gl_FragColor=tex;
+	fragColor=tex;
 }
