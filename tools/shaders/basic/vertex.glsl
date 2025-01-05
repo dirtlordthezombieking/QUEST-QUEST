d3b61a0151fd093670334b768f705e1a354dc1a9
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-04 21:32:45 -0400

    Update vertex.glsl

--- tools/shaders/basic/vertex.glsl
+++ tools/shaders/basic/vertex.glsl
@@ -2,9 +2,10 @@
 in vec4 a_data;
 out vec2 v_uv;
 uniform vec2 u_pos;
-uniform vec2 u_texOff;
+uniform float u_scale:
+uniform vec2 u_size
 void main()
 {
-	v_uv=a_data.zw+u_texOff;
-	gl_Position=vec4((a_data.xy+u_pos)/vec2(384.0,256.0),0.0,1.0);
+	v_uv=a_data.zw;
+	gl_Position=vec4(((a_data.xy*u_scale)+u_pos)/u_size,0.0,1.0);
 }
