639172fd2542d65feced178d03d5e8b821c2b5e3
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-04 21:28:20 -0400

    Update vertex.glsl

--- tools/shaders/basic/vertex.glsl
+++ tools/shaders/basic/vertex.glsl
@@ -1,10 +1,10 @@
 #version 300 es
-precision mediump float;
-out vec4 fragColor;
-in vec2 v_uv;
-uniform sampler2D u_tex;
+in vec4 a_data;
+out vec2 v_uv;
+uniform vec2 u_pos;
+uniform vec2 u_texOff;
 void main()
 {
-	vec4 tex=texture(u_tex,v_uv);
-	fragColor=tex;
+	v_uv=a_data.zw+u_texOff;
+	gl_Position=vec4((a_data.xy+u_pos)/vec2(384.0,256.0),0.0,1.0);
 }
