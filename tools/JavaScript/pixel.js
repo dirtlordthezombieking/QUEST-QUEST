32828d95d6a908f8536d6e53272ab3e24a44e214
dirtlordthezombieking <58833369+dirtlordthezombieking@users.noreply.github.com>
2025-01-07 15:57:07 -0400

    Update pixel.js

--- tools/JavaScript/pixel.js
+++ tools/JavaScript/pixel.js
@@ -24,10 +24,13 @@ function touchEnd(e)
 //--------------------------------------------------IMAGE FUNCTIONS
 function addLayer()
 {
-	layers.push({});
-	layers.arr=Array(width*height*4).fill(0);
-	updateLayer(layers.length);
-	layers++;
+	if(LayersCount>=layers.length);
+	{
+		layers.push({});
+	}
+	layers[LayersCount].arr=Array(width*height*4).fill(0);
+	updateLayer(LayersCount);
+	LayersCount++;
 }
 function updateLayer(layerID)
 {
@@ -46,14 +49,14 @@ function updateLayer(layerID)
 	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE);
 	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER,gl.NEAREST);
 	gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER,gl.NEAREST);
-	gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,width,height,0,gl.RGBA,gl.UNSIGNED_BYTE,new ()Uint8Array);
+	gl.texImage2D(gl.TEXTURE_2D,0,gl.RGBA,width,height,0,gl.RGBA,gl.UNSIGNED_BYTE,new Uint8Array(layers[layerId].texture));
 //----
 	saveGl.bindTexture(saveGl.TEXTURE_2D,layers[layerId].texture);
 	saveGl.texParameteri(saveGl.TEXTURE_2D,saveGl.TEXTURE_WRAP_S,saveGl.CLAMP_TO_EDGE);
 	saveGl.texParameteri(saveGl.TEXTURE_2D,saveGl.TEXTURE_WRAP_T,saveGl.CLAMP_TO_EDGE);
 	saveGl.texParameteri(saveGl.TEXTURE_2D,saveGl.TEXTURE_MIN_FILTER,saveGl.NEAREST);
 	saveGl.texParameteri(saveGl.TEXTURE_2D,saveGl.TEXTURE_MAG_FILTER,saveGl.NEAREST);
-	saveGl.texImage2D(saveGl.TEXTURE_2D,0,saveGl.RGBA,saveGl.RGBA,saveGl.UNSIGNED_BYTE);
+	saveGl.texImage2D(saveGl.TEXTURE_2D,0,saveGl.RGBA,width,height,0,saveGl.RGBA,saveGl.UNSIGNED_BYTE,new Uint8Array(layers[layerId].SaveTexture));
 }
 function setPixel(x,y,layerID)
 {
@@ -142,7 +145,7 @@ let width=64;
 let heght=64;
 let pointers=[];
 let layers=[];
-let Layers count=0;
+let LayersCount=0;
 let layerDraws=[];
 let layers=0;
 //---------------------------------------------PRIMARY CANVAS SETUP
